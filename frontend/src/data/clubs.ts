7e108ecc29804fdc1d1034e9be57244758575fef
David Yu <yu.dav@northeastern.edu>
2025-05-16 01:20:18 -0400

    add club logos to our club db

--- frontend/src/data/clubs.ts
+++ frontend/src/data/clubs.ts
@@ -9,55 +9,55 @@ export const clubs: Club[] = [
   {
     id: "c4c",
     name: "Code4Community",
-    logo: "/logos/c4c.png", // Replace with actual logo path
+    logo: "https://shorturl.at/Erttt",
     url: "https://www.c4cneu.com/",
   },
   {
     id: "generate",
     name: "Generate Product Development",
-    logo: "/logos/generate.png", // Replace with actual logo path
+    logo: "https://shorturl.at/QB26P",
     url: "https://generatenu.com/",
   },
   {
     id: "sandbox",
     name: "Sandbox",
-    logo: "/logos/sandbox.png", // Replace with actual logo path
+    logo: "https://shorturl.at/7IKOH",
     url: "https://www.sandboxnu.com/",
   },
   {
     id: "scout",
     name: "Scout",
-    logo: "/logos/scout.png", // Replace with actual logo path
+    logo: "https://shorturl.at/juGbJ",
     url: "https://scout.camd.northeastern.edu/",
   },
   {
     id: "ner",
     name: "Northeastern Electric Racing",
-    logo: "/logos/ner.png", // Replace with actual logo path
+    logo: "https://shorturl.at/iWMUy",
     url: "https://electricracing.northeastern.edu/",
   },
   {
     id: "forge",
     name: "Forge",
-    logo: "/logos/forge.png", // Replace with actual logo path
+    logo: "https://tinyurl.com/mr3mt82t",
     url: "https://www.forgenu.com/",
   },
   {
     id: "hoosky",
     name: "Hoosky",
-    logo: "/logos/hoosky.png", // Replace with actual logo path
+    logo: "https://tinyurl.com/4d7hd5z9",
     url: "https://hoosky.store/",
   },
   {
     id: "colorstack",
     name: "ColorStack",
-    logo: "/logos/colorstack.png", // Replace with actual logo path
+    logo: "https://tinyurl.com/yfnhxszv",
     url: "https://colorstackneu.sites.northeastern.edu/",
   },
   {
     id: "hbp",
     name: "HackBeanpot",
-    logo: "/logos/hackbeanpot.png", // Replace with actual logo path
+    logo: "https://tinyurl.com/592b3hne",
     url: "https://www.hackbeanpot.com/",
   },
 ];
